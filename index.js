0b30fb8de8a8162a0d40fcee926be9fc1ddd5d47
markwelc <markwelc@oregonstate.edu>
2021-02-25 14:13:27 -0800

    added lines to require cors and express

--- index.js
+++ index.js
@@ -1,6 +1,11 @@
 const express = require('express');
 const app = express();
 const mysql = require('mysql');
+const cors = require("cors");
+
+app.use(cors());
+app.use(express.json());
+
 const db = mysql.createConnection({
     user: "bde25e105415bc",
     password: "9d786746",
