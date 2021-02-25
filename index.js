cb75d7290adfd505d3fdc7f43c32f6d621323d74
Greg Stula <gregstula@me.com>
2021-02-25 14:45:59 -0500

    Add database connection to MySQL hosted via clearDB addon

--- index.js
+++ index.js
@@ -1,5 +1,13 @@
 const express = require('express');
 const app = express();
+const mysql = require('mysql');
+
+const db = mysql.createConnection({
+    user: "bde25e105415bc",
+    password: "id786746",
+    host: "us-cdbr-east-03.cleardb.com",
+    database: "heroku_da400718f3a8859"
+});
 
 let port = process.env.PORT;
 if (port == null || port == "") {
