e1379ded1771c40dd22732f25693f52677b0105d
Greg Stula <gregstula@me.com>
2021-02-25 14:52:16 -0500

    Fix db password

--- index.js
+++ index.js
@@ -1,10 +1,9 @@
 const express = require('express');
 const app = express();
 const mysql = require('mysql');
-
 const db = mysql.createConnection({
     user: "bde25e105415bc",
-    password: "id786746",
+    password: "9d786746",
     host: "us-cdbr-east-03.cleardb.com",
     database: "heroku_da400718f3a8859"
 });
